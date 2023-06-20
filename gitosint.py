69bdae9b48288f9e3d00218ce06d63522a8938ce
Niclas Älmeby <noreply>
2023-06-20 22:54:34 +0200

    added ability to search for words in repo

--- gitosint.py
+++ gitosint.py
@@ -5,13 +5,14 @@ import argparse
 import shutil
 import datetime
 import stat
-
+import re
 
 
 parser = argparse.ArgumentParser(description='Small GIT OSINT program')
 repo_source_type = parser.add_mutually_exclusive_group(required=True)
 repo_source_type.add_argument("-u", "--url", help="Url to repo")
 repo_source_type.add_argument("-r","--repo", help="Path to repo")
+parser.add_argument("-s","--search", help="Strings to search for. A single string or multiple seperated by comma")
 
 args = vars(parser.parse_args())
 
@@ -26,37 +27,60 @@ if git_url:
     repo = git.Repo.clone_from(git_url, repo_path)
 elif args["repo"]:
     repo_path =  os.path.realpath(args["repo"])
-    print("repo path: ", repo_path)
     git_name = os.path.basename(os.path.normpath(repo_path))
     repo = git.Repo(repo_path)
-
 
 # print(repo_path)
 
 # heads = repo.heads
 # master = heads.master
 
-commits = repo.iter_commits()
-
-committers = dict()
-
-for commit in commits:
-    commit_date = commit.committed_datetime
-    date = datetime.datetime(commit_date.year, commit_date.month, commit_date.day, commit_date.hour, commit_date.minute, commit_date.second)
-    commit_data = {"hex": commit.hexsha, "date": commit_date}
-
-    if commit.author in committers:
-        committers[commit.author].append(commit_data)
-    else:
-        committers[commit.author] = [commit_data]
-
 
 # TODO create first/latest commit
-# TODO make it possible to change the values that are outputed
 
+def get_commit_info():
+
+    commits = repo.iter_commits()
+
+    committers = dict()
+
+    for commit in commits:
+        commit_date = commit.committed_datetime
+        date = datetime.datetime(commit_date.year, commit_date.month, commit_date.day, commit_date.hour, commit_date.minute, commit_date.second)
+        commit_data = {"hex": commit.hexsha, "date": commit_date}
+
+        if commit.author in committers:
+            committers[commit.author].append(commit_data)
+        else:
+            committers[commit.author] = [commit_data]
+
+    return committers
+
+
+def find_text_in_repo():
+
+    strings_to_find = str(args["search"]).split(",")
+    commits = repo.iter_commits()
+    for commit in commits:
+
+        for blob in commit.tree.blobs:
+            file_content = blob.data_stream.read().decode("utf-8")
+
+            for string in strings_to_find:
+                find_str = f"({string}.*\\n)"
+                # print(find_str)
+                regex = re.findall(find_str, file_content)
+
+                print(commit.hexsha, find_str, blob.path, regex)
+
+find_text_in_repo()
+
+
+commits = get_commit_info()
+# TODO make it possible to change the values that are outputed
 print("Email,Username,Number of commits")
 print("--------------------------------")
-for key, value in committers.items():
+for key, value in commits.items():
     print(f"{key.email},{key.name},{len(value)}")
 
 repo.close()
@@ -70,3 +94,6 @@ def onerror(func, path, exc_info):
 
 if args["url"]:
     shutil.rmtree(osint_path, onerror=onerror)
+
+
+# TODO use https://github.com/ishepard/pydriller instead of gitpython. It seems alot easier to use
